2e547fa87b68deebc2521e8cd6c81a99b138010d
Nguyen Duc Chung <nchung1020@gmail.com>
2024-08-09 15:19:46 +0700

    Push API UC1,2

---
+++ QuanLyNhanSu/QuanLyNhanSu.WebAPI/Program.cs
@@ -0,0 +1,50 @@
+using Microsoft.EntityFrameworkCore;
+using QuanLyNhanSu.Business.Interfaces;
+using QuanLyNhanSu.Business.Services;
+using QuanLyNhanSu.Data.Context;
+using QuanLyNhanSu.Data.Interfaces;
+using QuanLyNhanSu.Data.Repositories;
+using QuanLyNhanSu.Models.Entities;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+
+namespace QuanLyNhanSu.WebAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddDbContext<QLNSContext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"), b => b.MigrationsAssembly("QuanLyNhanSu.WebAPI")));
+            // Add services to the container.
+
+            builder.Services.AddScoped(typeof(IBaseRepository<>), typeof(BaseRepository<>));
+            builder.Services.AddScoped<IEmployeeService, EmployeeService>();
+            builder.Services.AddScoped<IChamCongService, ChamCongService>();
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
