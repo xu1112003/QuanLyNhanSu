69c73148a34e752e17f1e87178edd387c1b62644
xu1112003 <phungchien30031993@gmail.com>
2024-08-13 14:00:02 +0700

    merger

---
+++ QuanLyNhanSu.WebAPI/Controllers/UsersController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using QuanLyNhanSu.Business.Interfaces;
+using QuanLyNhanSu.Models.DTO;
+
+namespace QuanLyNhanSu.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController: ControllerBase
+    {
+        private readonly IUserService _userService;
+        public UsersController(IUserService userService) {
+            _userService = userService;
+        }
+        //[HttpPost("authenticate")]
+        //[AllowAnonymous]
+        //public async Task<IActionResult> Autheticate([FromBody] LoginRequest request)
+        //{
+        //    if(!ModelState.IsValid)
+        //    {
+        //        return BadRequest(ModelState);
+        //    }
+        //    var resultToken = await _userService.Authencate(request);
+        //    if(string.IsNullOrEmpty(resultToken))
+        //    {
+        //        return BadRequest("Username or pasword is incorrect");
+        //    }
+        //    return Ok(new {token = resultToken});
+        //}
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Register([FromBody] RegisterRequest request)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var result = await _userService.Register(request);
+            if (!result)
+            {
+                return BadRequest("Register is unsuccessful");
+            }
+            return Ok();
+        }
+
+
+    }
+}
--- QuanLyNhanSu.WebAPI/Program.cs
+++ QuanLyNhanSu.WebAPI/Program.cs
@@ -1,11 +1,13 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using QuanLyNhanSu.Business.Interfaces;
 using QuanLyNhanSu.Business.Services;
 using QuanLyNhanSu.Data.Context;
 using QuanLyNhanSu.Data.Interfaces;
 using QuanLyNhanSu.Data.Repositories;
 using QuanLyNhanSu.Models.Entities;
-using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+
 
 namespace QuanLyNhanSu.WebAPI
 {
@@ -17,19 +19,37 @@ namespace QuanLyNhanSu.WebAPI
 
             builder.Services.AddDbContext<QLNSContext>(options =>
             options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"), b => b.MigrationsAssembly("QuanLyNhanSu.WebAPI")));
+            builder.Services.AddIdentity<User,Role>()
+            .AddEntityFrameworkStores<QLNSContext>()
+            .AddDefaultTokenProviders();
+
+
             // Add services to the container.
             builder.Services.AddScoped(typeof(IBaseRepository<>), typeof(BaseRepository<>));
             builder.Services.AddScoped(typeof(IScheduleRepository<>), typeof(ScheduleRepository<>));
             builder.Services.AddScoped<IScheduleService, ScheduleService>();
             builder.Services.AddScoped(typeof(IAccountRepository<>), typeof(AccountRepository<>));
             builder.Services.AddTransient<IAccountService, AccountService>();
+            builder.Services.AddTransient<UserManager<User>, UserManager<User>>();
+            builder.Services.AddTransient<SignInManager<User>, SignInManager<User>>();
+            builder.Services.AddTransient<RoleManager<Role>, RoleManager<Role>>();
+            //builder.Services.AddScoped<IUserService, UserService>();
+
+
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
-
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAllOrigins",
+                    builder =>
+                    {
+                        builder.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin();
+                    });
+            });
             var app = builder.Build();
-
+            app.UseCors("AllowAllOrigins");
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
@@ -40,7 +60,7 @@ namespace QuanLyNhanSu.WebAPI
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
-
+            app.UseAuthentication();
 
             app.MapControllers();
 
